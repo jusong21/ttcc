82c25c864e76d4fba488a2aac74da45df4f9e7d4
Juhee <song.of.may21@gmail.com>
2024-12-16 13:47:16 +0100

    ntuplizer with puppi

--- BTVNanoCommissioning/src/BTVNanoCommissioning/workflows/ttcc_ntuplizer.py
+++ BTVNanoCommissioning/src/BTVNanoCommissioning/workflows/ttcc_ntuplizer.py
@@ -59,6 +59,7 @@ class NanoProcessor(processor.ProcessorABC):
         isRealData = not hasattr(events, "weight")
         #isTTbar = 'TTTo' in dataset
         isTTbar = ("TTTo" in dataset) or ("TTbb" in dataset)
+        print("dataset:", dataset)
 
         ## Create histograms
         _hist_event_dict = (
@@ -75,7 +76,7 @@ class NanoProcessor(processor.ProcessorABC):
         else:
             output["sumw"] = ak.sum(events.weight)
 
-        req_nbjet = events.nbJets >= 2
+        req_nbjet = events.nbJetsM >= 2
         #req_nbjet = events.nbJets >= 0
         #njets = ak.num(events.Jet)
         #jets = events.Jet[:,:4] # use only 4 jets
@@ -86,191 +87,14 @@ class NanoProcessor(processor.ProcessorABC):
         leptons = events.Lepton[req_nbjet]
         #dr_leptons = leptons[:,0].delta_r(leptons[:,1])
         met = events.MET[req_nbjet]
-
-        # genJets category
-        genbjet = events.Jet[events.Jet.hadronFlavour==5]
-        gencjet = events.Jet[events.Jet.hadronFlavour==4]
-        genlfjet = events.Jet[events.Jet.hadronFlavour==0]
-        
-        ngenbjets = ak.num(genbjet)
-        ngencjets = ak.num(gencjet)
-        ngenlfjets = ak.num(genlfjet)
-
-        ttbar_criteria = {
-            'ttbb': ngenbjets >= 4,
-            'ttbj': ngenbjets == 3,
-            'ttcc': (ngenbjets == 2) & (ngencjets >= 2),
-            'ttcj': (ngenbjets == 2) & (ngencjets == 1),
-            'ttother': (ngenbjets == 2) & (ngencjets == 0) & (ngenlfjets >=2),
-        }
             
-        zeros = ak.zeros_like(events.nbJets, dtype=bool)
-        isttbb_new, isttbj_new = zeros, zeros
-        isttcc_new, isttcj_new = zeros, zeros
-        isttother_new = zeros
-
-        #category_genJets = ak.full(events.nbJets, 4)
-        category_genJets = ak.Array(np.full(len(events.nbJets), 4))
-        category_genJets = ak.to_numpy(
-            ak.where(
-                ttbar_criteria["ttbb"],
-                0,
-                category_genJets,
-            )
-        )
-        category_genJets = ak.to_numpy(
-            ak.where(
-                ttbar_criteria["ttbj"],
-                1,
-                category_genJets,
-            )
-        )
-        category_genJets = ak.to_numpy(
-            ak.where(
-                ttbar_criteria["ttcc"],
-                2,
-                category_genJets,
-            )
-        )
-        category_genJets = ak.to_numpy(
-            ak.where(
-                ttbar_criteria["ttcj"],
-                3,
-                category_genJets,
-            )
-        )
-            
-            
-        isttbb_new = ak.to_numpy(
-            ak.where(
-                ttbar_criteria['ttbb'],
-                True,
-                isttbb_new,
-            )
-        )
-        isttbj_new = ak.to_numpy(
-            ak.where(
-                ttbar_criteria['ttbj'],
-                True, 
-                isttbj_new,
-            )
-        )
-        isttcc_new = ak.to_numpy(
-            ak.where(
-                ttbar_criteria['ttcc'],
-                True,
-                isttcc_new,
-            )
-        )
-        isttcj_new = ak.to_numpy(
-            ak.where(
-                ttbar_criteria['ttcj'],
-                True,
-                isttcj_new,
-            )
-        )
-        isttother_new = ak.to_numpy(
-            ak.where(
-                (~isttbb_new & ~isttbj_new & ~isttcc_new & ~isttcj_new),
-                #ttbar_criteria['ttother'],
-                True,
-                isttother_new,
-            )
-        )
-
-        # genTtbarId category
-        genTtbarId = events.genTtbarId
-
-        ttbar_criteria2 = {
-            'ttbb': (genTtbarId%100 == 53) | (genTtbarId%100 == 54) | (genTtbarId%100 == 55),
-            'ttbj': (genTtbarId%100 == 51) | (genTtbarId%100 == 52),
-            'ttcc': (genTtbarId%100 == 43) | (genTtbarId%100 == 44) | (genTtbarId%100 == 45),
-            'ttcj': (genTtbarId%100 == 41) | (genTtbarId%100 == 42),
-            'ttother': (genTtbarId%100 == 0),
-        }
-        #category_genTtbarId = ak.full(events.nbJets, 5)
-        category_genTtbarId = ak.Array(np.full(len(events.nbJets), 5))
-        category_genTtbarId = ak.to_numpy(
-            ak.where(
-                ttbar_criteria2["ttbb"],
-                0,
-                category_genTtbarId,
-            )
-        )
-        category_genTtbarId = ak.to_numpy(
-            ak.where(
-                ttbar_criteria2["ttbj"],
-                1,
-                category_genTtbarId,
-            )
-        )
-        category_genTtbarId = ak.to_numpy(
-            ak.where(
-                ttbar_criteria2["ttcc"],
-                2,
-                category_genTtbarId,
-            )
-        )
-        category_genTtbarId = ak.to_numpy(
-            ak.where(
-                ttbar_criteria2["ttcj"],
-                3,
-                category_genTtbarId,
-            )
-        )
-        category_genTtbarId = ak.to_numpy(
-            ak.where(
-                ttbar_criteria2["ttother"],
-                4,
-                category_genTtbarId,
-            )
-        )
-            
-        category_addJet = ak.Array(np.full(len(events.nbJets), 5))
-        category_addJet = ak.to_numpy(
-            ak.where(
-                events.isttbb,
-                0,
-                category_addJet,
-            )
-        )
-        category_addJet = ak.to_numpy(
-            ak.where(
-                events.isttbj,
-                1,
-                category_addJet,
-            )
-        )
-        category_addJet = ak.to_numpy(
-            ak.where(
-                events.isttcc,
-                2,
-                category_addJet,
-            )
-        )
-        category_addJet = ak.to_numpy(
-            ak.where(
-                events.isttcj,
-                3,
-                category_addJet,
-            )
-        )
-        category_addJet = ak.to_numpy(
-            ak.where(
-                events.isttother,
-                4,
-                category_addJet,
-            )
-        )
-            
-            
-        jet_drLep1 = ak.to_numpy(jets_bsort.delta_r(leptons[:,0]))
-        jet_drLep2 = ak.to_numpy(jets_bsort.delta_r(leptons[:,1]))
-        
-        lep_drJet1 = np.column_stack((jet_drLep1[:, 0], jet_drLep2[:, 0]))
-        lep_drJet2 = np.column_stack((jet_drLep1[:, 1], jet_drLep2[:, 1]))
-        lep_drJet3 = np.column_stack((jet_drLep1[:, 2], jet_drLep2[:, 2]))
-        lep_drJet4 = np.column_stack((jet_drLep1[:, 3], jet_drLep2[:, 3]))
+#        jet_drLep1 = ak.to_numpy(jets_bsort.delta_r(leptons[:,0]))
+#        jet_drLep2 = ak.to_numpy(jets_bsort.delta_r(leptons[:,1]))
+#        
+#        lep_drJet1 = np.column_stack((jet_drLep1[:, 0], jet_drLep2[:, 0]))
+#        lep_drJet2 = np.column_stack((jet_drLep1[:, 1], jet_drLep2[:, 1]))
+#        lep_drJet3 = np.column_stack((jet_drLep1[:, 2], jet_drLep2[:, 2]))
+#        lep_drJet4 = np.column_stack((jet_drLep1[:, 3], jet_drLep2[:, 3]))
 
         #lepton_drJet1 = transposed[:, 0]  # shape: (2,)
         #lepton_drJet2 = transposed[:, 1]  # shape: (2,)
@@ -283,122 +107,94 @@ class NanoProcessor(processor.ProcessorABC):
         #lep_drJet4 = leptons.delta_r(jets_bsort[:,3])
 
         #pruned_ev = {'Channel': ak.to_numpy(events.Channel), 'nJets': ak.to_numpy(njets), 'nbJets': ak.to_numpy(events.nbJets), 'nbJets_T': ak.to_numpy(events.nbJets_T), 'ncJets': ak.to_numpy(events.ncJets), 'ncJets_T': ak.to_numpy(events.ncJets_T)}
-        pruned_ev = {'sortJet': jets_bsort, 'Jet': jets[req_nbjet], 'Lepton': leptons, 'Channel': ak.to_numpy(events.Channel[req_nbjet]), 'nJets': ak.to_numpy(events.nJets[req_nbjet]), 'nbJets': ak.to_numpy(events.nbJets[req_nbjet]), 'nbJets_T': ak.to_numpy(events.nbJetsT[req_nbjet]), 'ncJets': ak.to_numpy(events.ncJets[req_nbjet]), 'ncJets_T': ak.to_numpy(events.ncJetsT[req_nbjet]), "ngenbJets": ak.to_numpy(ngenbjets[req_nbjet]), "ngencJets": ak.to_numpy(ngencjets[req_nbjet]), "ngenlfJets": ak.to_numpy(ngenlfjets[req_nbjet]), }
+        pruned_ev = {'sortJet': jets_bsort, 'Jet': jets[req_nbjet], 'Lepton': leptons, 'Channel': events.Channel[req_nbjet], 'nJets': events.nJets[req_nbjet], 'nbJetsL': events.nbJetsL[req_nbjet], "nbJetsM": events.nbJetsM[req_nbjet], 'nbJetsT': events.nbJetsT[req_nbjet], 'ncJetsL': events.ncJetsL[req_nbjet], 'ncJetsM': events.ncJetsM[req_nbjet], 'ncJetsT': events.ncJetsT[req_nbjet], }
 
         # leptons
 #        #pruned_ev['drLepton12'] = ak.to_numpy(dr_leptons)
         pruned_ev['massLepton12'] = ak.to_numpy(events.mll[req_nbjet])
-        pruned_ev['Lepton_drJet1'] = ak.to_numpy(lep_drJet1)
-        pruned_ev['Lepton_drJet2'] = ak.to_numpy(lep_drJet2)
-        pruned_ev['Lepton_drJet3'] = ak.to_numpy(lep_drJet3)
-        pruned_ev['Lepton_drJet4'] = ak.to_numpy(lep_drJet4)
+#        pruned_ev['Lepton_drJet1'] = ak.to_numpy(lep_drJet1)
+#        pruned_ev['Lepton_drJet2'] = ak.to_numpy(lep_drJet2)
+#        pruned_ev['Lepton_drJet3'] = ak.to_numpy(lep_drJet3)
+#        pruned_ev['Lepton_drJet4'] = ak.to_numpy(lep_drJet4)
 
         # MET
         pruned_ev['MET'] = ak.to_numpy(met.pt)
         pruned_ev['MET_phi'] = ak.to_numpy(met.phi)
 
         # jets
-        pruned_ev['sortJet_drLep1'] = jet_drLep1
-        pruned_ev['sortJet_drLep2'] = jet_drLep2
+#        pruned_ev['sortJet_drLep1'] = jet_drLep1
+#        pruned_ev['sortJet_drLep2'] = jet_drLep2
 
         # dijet
-        pruned_ev['drJet12'] = ak.to_numpy(jets_bsort[:,0].delta_r(jets_bsort[:,1]))
-        pruned_ev['drJet13'] = ak.to_numpy(jets_bsort[:,0].delta_r(jets_bsort[:,2]))
-        pruned_ev['drJet14'] = ak.to_numpy(jets_bsort[:,0].delta_r(jets_bsort[:,3]))
-        pruned_ev['drJet23'] = ak.to_numpy(jets_bsort[:,1].delta_r(jets_bsort[:,2]))
-        pruned_ev['drJet24'] = ak.to_numpy(jets_bsort[:,1].delta_r(jets_bsort[:,3]))
-        pruned_ev['drJet34'] = ak.to_numpy(jets_bsort[:,2].delta_r(jets_bsort[:,3]))
+#        pruned_ev['drJet12'] = ak.to_numpy(jets_bsort[:,0].delta_r(jets_bsort[:,1]))
+#        pruned_ev['drJet13'] = ak.to_numpy(jets_bsort[:,0].delta_r(jets_bsort[:,2]))
+#        pruned_ev['drJet14'] = ak.to_numpy(jets_bsort[:,0].delta_r(jets_bsort[:,3]))
+#        pruned_ev['drJet23'] = ak.to_numpy(jets_bsort[:,1].delta_r(jets_bsort[:,2]))
+#        pruned_ev['drJet24'] = ak.to_numpy(jets_bsort[:,1].delta_r(jets_bsort[:,3]))
+#        pruned_ev['drJet34'] = ak.to_numpy(jets_bsort[:,2].delta_r(jets_bsort[:,3]))
 
 #        # btag sorting jets
 
         out_branch = ["events", "run", "luminosityBlock"]
         if isTTbar:
             pruned_ev.update({
-                'isttbb': ak.to_numpy(events.isttbb[req_nbjet]), 
-                'isttbj': ak.to_numpy(events.isttbj[req_nbjet]), 
-                'isttcc': ak.to_numpy(events.isttcc[req_nbjet]), 
-                'isttcj': ak.to_numpy(events.isttcj[req_nbjet]), 
-                'isttother': ak.to_numpy(events.isttother[req_nbjet]),
-                'isttbb_new': isttbb_new[req_nbjet], 
-                'isttbj_new': isttbj_new[req_nbjet], 
-                'isttcc_new': isttcc_new[req_nbjet], 
-                'isttcj_new': isttcj_new[req_nbjet], 
-                'isttother_new': isttother_new[req_nbjet],
-                "category_genJets": category_genJets[req_nbjet],
-                "category_genTtbarId": category_genTtbarId[req_nbjet],
-                "genTtbarId": ak.to_numpy(genTtbarId[req_nbjet]),
-		"category_addJet": category_addJet[req_nbjet],
-		"nbJetsFromT": ak.to_numpy(events.nbJetsFromT[req_nbjet]),
-		"nbJetsFromW": ak.to_numpy(events.nbJetsFromW[req_nbjet]),
-		"ncJetsFromW": ak.to_numpy(events.ncJetsFromW[req_nbjet]),
-		"naddbJets": ak.to_numpy(events.naddbJets[req_nbjet]),
-		"naddcJets": ak.to_numpy(events.naddcJets[req_nbjet]),
-                "bJetFromT": events.bJetFromT[req_nbjet],
-                "bJetFromW": events.bJetFromW[req_nbjet],
-                "cJetFromW": events.cJetFromW[req_nbjet],
-                "addbJet": events.addbJet[req_nbjet],
-                "addcJet": events.addcJet[req_nbjet],
-                "addlfJet": events.addlfJet[req_nbjet],
-                "nGenJets": ak.to_numpy(events.nGenJets[req_nbjet]),
-
-
+                "ttbar_category": events.ttbar.category[req_nbjet],
+                "genTtbarId": events.genTtbarId[req_nbjet],
+            #    "nGenJets": ak.to_numpy(events.nGenJets[req_nbjet]),
             })
 
         if not isRealData:
             print("is not RealData")
-            DeepJetC_totDown_weight, DeepJetC_totUp_weight = calc_tot_unc(events, "DeepJetCJet")
-            DeepJetB_totDown_weight, DeepJetB_totUp_weight = calc_tot_unc(events, "DeepJetBJet")
+#            DeepJetC_totDown_weight, DeepJetC_totUp_weight = calc_tot_unc(events, "DeepJetCJet")
+#            DeepJetB_totDown_weight, DeepJetB_totUp_weight = calc_tot_unc(events, "DeepJetBJet")
 
             pruned_ev.update({
-               "weight": ak.to_numpy(events.weight[req_nbjet]),
-               "genweight_weight": ak.to_numpy(events.genweight.weight[req_nbjet]),
-               "puweight_weight": ak.to_numpy(events.puweight.weight[req_nbjet]),
-               "HLT_weight": ak.to_numpy(events.HLT.weight[req_nbjet]),
-               "mu_Reco_weight": ak.to_numpy(events.mu.Reco_weight[req_nbjet]),
-               "mu_ID_weight": ak.to_numpy(events.mu.ID_weight[req_nbjet]),
-               "mu_Iso_weight": ak.to_numpy(events.mu.Iso_weight[req_nbjet]),
-               "ele_Reco_weight": ak.to_numpy(events.ele.Reco_weight[req_nbjet]),
-               "ele_ID_weight": ak.to_numpy(events.ele.ID_weight[req_nbjet]),
-               "L1PreFiring_weight": ak.to_numpy(events.L1PreFiringWeight.Nom[req_nbjet]),
-               "DeepJetC_weight": ak.to_numpy(ak.prod(events.DeepJetCJet.weight[req_nbjet], axis=-1)),
-               "DeepJetB_weight": ak.to_numpy(ak.prod(events.DeepJetBJet.weight[req_nbjet], axis=-1)),
+               "weight": events.weight[req_nbjet],
+               "genweight_weight": events.genweight.weight[req_nbjet],
+               "puweight_weight": events.puweight.weight[req_nbjet],
+               "HLT_weight": events.HLT.weight[req_nbjet],
+               "mu_Reco_weight": events.mu.Reco_weight[req_nbjet],
+               "mu_ID_weight": events.mu.ID_weight[req_nbjet],
+               "mu_Iso_weight": events.mu.Iso_weight[req_nbjet],
+               "ele_Reco_weight": events.ele.Reco_weight[req_nbjet],
+               "ele_ID_weight": events.ele.ID_weight[req_nbjet],
+               "L1PreFiring_weight": events.L1PreFiringWeight.Nom[req_nbjet],
+               "PNet_weight": events.PNet.weight[req_nbjet],
+#               "DeepJetC_weight": ak.to_numpy(ak.prod(events.DeepJetCJet.weight[req_nbjet], axis=-1)),
+#               "DeepJetB_weight": ak.to_numpy(ak.prod(events.DeepJetBJet.weight[req_nbjet], axis=-1)),
 
                # up
-               "puweightUp_weight": ak.to_numpy(events.puweightUp.weight[req_nbjet]),
-               "HLTUp_weight": ak.to_numpy(events.HLTUp.weight[req_nbjet]),
-               "mu_RecoUp_weight": ak.to_numpy(events.mu.RecoUp_weight[req_nbjet]),
-               "mu_IDUp_weight": ak.to_numpy(events.mu.IDUp_weight[req_nbjet]),
-               "mu_IsoUp_weight": ak.to_numpy(events.mu.IsoUp_weight[req_nbjet]),
-               "ele_RecoUp_weight": ak.to_numpy(events.ele.RecoUp_weight[req_nbjet]),
-               "ele_IDUp_weight": ak.to_numpy(events.ele.IDUp_weight[req_nbjet]),
-               "DeepJetC_TotUp_weight": DeepJetC_totUp_weight[req_nbjet],
-               "DeepJetB_TotUp_weight": DeepJetB_totUp_weight[req_nbjet],
-##               "DeepJetC_TotUp_weight": ak.to_numpy(DeepJetC_totUp_weight),
-##               "DeepJetB_TotUp_weight": ak.to_numpy(DeepJetB_totUp_weight),
+               "puweightUp_weight": events.puweightUp.weight[req_nbjet],
+               "HLTUp_weight": events.HLTUp.weight[req_nbjet],
+               "mu_RecoUp_weight": events.mu.RecoUp_weight[req_nbjet],
+               "mu_IDUp_weight": events.mu.IDUp_weight[req_nbjet],
+               "mu_IsoUp_weight": events.mu.IsoUp_weight[req_nbjet],
+               "ele_RecoUp_weight": events.ele.RecoUp_weight[req_nbjet],
+               "ele_IDUp_weight": events.ele.IDUp_weight[req_nbjet],
+#               "DeepJetC_TotUp_weight": DeepJetC_totUp_weight[req_nbjet],
+#               "DeepJetB_TotUp_weight": DeepJetB_totUp_weight[req_nbjet],
                # need to check
-               "L1PreFiringUp_weight": ak.to_numpy(events.L1PreFiringWeight.Dn[req_nbjet]),
+               "L1PreFiringUp_weight": events.L1PreFiringWeight.Dn[req_nbjet],
 #
 #
                # down
-               "puweightDown_weight": ak.to_numpy(events.puweightDown.weight[req_nbjet]),
-               "HLTDown_weight": ak.to_numpy(events.HLTDown.weight[req_nbjet]),
-               "mu_RecoDown_weight": ak.to_numpy(events.mu.RecoDown_weight[req_nbjet]),
-               "mu_IDDown_weight": ak.to_numpy(events.mu.IDDown_weight[req_nbjet]),
-               "mu_IsoDown_weight": ak.to_numpy(events.mu.IsoDown_weight[req_nbjet]),
-               "ele_RecoDown_weight": ak.to_numpy(events.ele.RecoDown_weight[req_nbjet]),
-               "ele_IDDown_weight": ak.to_numpy(events.ele.IDDown_weight[req_nbjet]),
-               "DeepJetC_TotDown_weight": DeepJetC_totDown_weight[req_nbjet],
-               "DeepJetB_TotDown_weight": DeepJetB_totDown_weight[req_nbjet],
-##               "DeepJetC_TotDown_weight": ak.to_numpy(DeepJetC_totDown_weight),
-##               "DeepJetB_TotDown_weight": ak.to_numpy(DeepJetB_totDown_weight),
+               "puweightDown_weight": events.puweightDown.weight[req_nbjet],
+               "HLTDown_weight": events.HLTDown.weight[req_nbjet],
+               "mu_RecoDown_weight": events.mu.RecoDown_weight[req_nbjet],
+               "mu_IDDown_weight": events.mu.IDDown_weight[req_nbjet],
+               "mu_IsoDown_weight": events.mu.IsoDown_weight[req_nbjet],
+               "ele_RecoDown_weight": events.ele.RecoDown_weight[req_nbjet],
+               "ele_IDDown_weight": events.ele.IDDown_weight[req_nbjet],
+#               "DeepJetC_TotDown_weight": DeepJetC_totDown_weight[req_nbjet],
+#               "DeepJetB_TotDown_weight": DeepJetB_totDown_weight[req_nbjet],
                # need to check
-               "L1PreFiringDown_weight": ak.to_numpy(events.L1PreFiringWeight.Up[req_nbjet]),
+               "L1PreFiringDown_weight": events.L1PreFiringWeight.Up[req_nbjet],
 #
             })
 
-            for kin in ["pt", "eta"]:
-                for obj in ["bJetFromT", "bJetFromW", "addbJet", "addcJet", "addlfJet"]:
-                    out_branch = np.append(out_branch, [f"{obj}_{kin}"])
+#            for kin in ["pt", "eta"]:
+#                for obj in ["bJetFromT", "bJetFromW", "addbJet", "addcJet", "addlfJet"]:
+#                    out_branch = np.append(out_branch, [f"{obj}_{kin}"])
 #"""
         out_branch = np.append(out_branch, list(pruned_ev.keys()))
         #out_branch = list(pruned_ev.keys())
@@ -411,6 +207,10 @@ class NanoProcessor(processor.ProcessorABC):
         foutname = f"{self.name}/{dataset}/{events.metadata['filename'].split('_')[-1].replace('.root','')}_{int(events.metadata['entrystop']/self.chunksize)}.root"
 
         if isRealData: foutname = f"{self.name}/{dataset}/{events.metadata['filename'].split('/')[-1].replace('.root','')}_{int(events.metadata['entrystop']/self.chunksize)}.root"
+
+        if len(pruned_ev["nJets"]) == 0:
+            print(f"Skipping writing {dataset} {foutname} as all branches are empty.")
+            return {dataset: output}
 
         with uproot.recreate( foutname) as fout:
             fout["Events"] = uproot_writeable(pruned_ev, include=out_branch)
